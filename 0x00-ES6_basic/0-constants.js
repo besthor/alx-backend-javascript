e4dbaee96747bd91bf2e44a7449d93a1ec8b7676
besthor <igbebestor7@gmail.com>
2023-06-26 15:09:49 +0000

    I prefer const

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -14,20 +14,3 @@ export function taskNext() {
 
 	return combination;
 }
-=======
-export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
-}
-
-export function getLast() {
-  return ' is okay';
-}
-
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
->>>>>>> 86bd64eae74f9b2cdc96b157fe6eafb1cfe5f066
