b56765073c18dd9016d994c6214558a585bc2e3c
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-26 23:58:59 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-  /* eslint-enable no-unused-vars */
 
   return [task, task2];
 }
