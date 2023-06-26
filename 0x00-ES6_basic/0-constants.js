ac3fcb39faa0b71b3fb52f3d0bae136021201c0f
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-26 15:03:03 +0100

    Update 0-constants.js

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+function taskFirst() {
+const task = 'I prefer const when I can.';
+return task;
+}
+
+export function getLast() {
+return ' is okay';
+}
+
+export function taskNext() {
+let combination = 'But sometimes let';
+combination += getLast();
+
+return combination;
+}
