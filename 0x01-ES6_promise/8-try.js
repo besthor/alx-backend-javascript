3ac235e029cdaf5098bbb0c2c0767a795e473a59
besthor <igbebestor7@gmail.com>
2023-06-28 10:55:09 +0000

    Throw error / try catch 2

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,3 @@
-//Write a function named divideFunction that will accept two arguments: numerator (Number)  and denominator (Number).
-//When the denominator argument is equal to 0, the function should throw a new error with the message cannot divide by 0.
-//Otherwise it should return the numerator divided by the denominator.
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw Error('cannot divide by 0');
