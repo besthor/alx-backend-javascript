b6f898c2b7fc074477eb0e9e595aa97a908612a5
besthor <igbebestor7@gmail.com>
2023-08-28 14:05:54 +0000

    main files

---
+++ 0x05-Node_JS_basic/3-main_0.js
@@ -0,0 +1,9 @@
+onst countStudents = require('./3-read_file_async');
+
+countStudents("nope.csv")
+    .then(() => {
+        console.log("Done!");
+    })
+        .catch((error) => {
+        console.log(error);
+    });
