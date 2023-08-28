ce5a7f647995a5188a4421a79559bde87cc626b2
besthor <igbebestor7@gmail.com>
2023-08-28 23:45:04 +0000

    a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
+
+module.exports = app;
