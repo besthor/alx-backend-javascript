cae0b700b2d0cc8128e9d87ea9da4e31b9fbe6ea
besthor <igbebestor7@gmail.com>
2023-08-31 13:17:48 +0000

    Spies

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,21 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    const aRound = Math.round(a);
+    const bRound = Math.round(b);
+
+    if (type === 'SUBTRACT') {
+      return aRound - bRound;
+    }
+
+    if (type === 'DIVIDE') {
+      if (bRound === 0) {
+        return 'Error';
+      }
+      return aRound / bRound;
+    }
+
+    return aRound + bRound;
+  },
+};
+
+module.exports = Utils;
